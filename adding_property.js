829311e2f93952f8371227014539d72b52017ca6
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-11-04 09:46:44 +0300

    🍵commenting my code

--- adding_property.js
+++ adding_property.js
@@ -1,3 +1,4 @@
+// creating a person object
 function person(name,age){
     this.name =name;
     this.age = this.age;
@@ -8,8 +9,16 @@ function person(name,age){
 
 }
 
+// instantiating the object
 const karim = new person("Karim", 27);
 karim.call();
 
+// adding a property to the object
 karim.gender = "male";
 console.log (`${karim.name} is a ${karim.gender}`)
+
+// alternative way of adding a property to an object
+
+karim['race']='African';
+
+console.log(`${karim.name} is an ${karim.race}`);
