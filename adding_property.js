d67ebb50750b6cfab8bdd0a7c60f09ee9f19ae9d
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-11-04 10:01:15 +0300

    🙆‍♂️🙆‍♂️🙆‍♂️

--- adding_property.js
+++ adding_property.js
@@ -1,12 +1,11 @@
 // creating a person object
-function person(name,age){
-    this.name =name;
-    this.age = this.age;
+function person(name, age) {
+  this.name = name;
+  this.age = this.age;
 
-    this.call = function(){
-        console.log(`${name} is ${age} years old`);
-    }
-
+  this.call = function () {
+    console.log(`${name} is ${age} years old`);
+  };
 }
 
 // instantiating the object
@@ -15,10 +14,28 @@ karim.call();
 
 // adding a property to the object
 karim.gender = "male";
-console.log (`${karim.name} is a ${karim.gender}`)
+console.log(`${karim.name} is a ${karim.gender}`);
 
 // alternative way of adding a property to an object
+// using bracket notation
 
-karim['race']='African';
+karim["race"] = "African";
 
 console.log(`${karim.name} is an ${karim.race}`);
+
+// deleting properties from an object
+for(let key in karim){
+    console.log(key);
+};
+console.log(`******************************************************`)
+delete karim["race"];
+
+for (let key in karim) {
+  console.log(key);
+};
+
+// iterating through properties of an object
+
+for(let key in person){
+console.log(key,  karim['key']);
+};
